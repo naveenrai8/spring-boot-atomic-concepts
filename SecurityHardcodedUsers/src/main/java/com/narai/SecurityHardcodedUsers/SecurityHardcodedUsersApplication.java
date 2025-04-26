1483532e4a44e74beaa0edbe4850aa09841ed452
Naveen Rai <naveenrai8@gmail.com>
2025-04-26 17:57:42 +0530

    added security using hardcoded user and password info

---
+++ SecurityHardcodedUsers/src/main/java/com/narai/SecurityHardcodedUsers/SecurityHardcodedUsersApplication.java
@@ -0,0 +1,13 @@
+package com.narai.SecurityHardcodedUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityHardcodedUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityHardcodedUsersApplication.class, args);
+	}
+
+}
---
+++ SecurityHardcodedUsers/src/test/java/com/narai/SecurityHardcodedUsers/SecurityHardcodedUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.narai.SecurityHardcodedUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityHardcodedUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
